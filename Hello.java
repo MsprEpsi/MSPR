278e58d6f44c7c223fc5ceccf62dcb9aa412963d
Mathieu Papazian <mathieu.papazian46@gmail.com>
2022-01-03 14:34:19 +0100

    updateHelloWorld

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello{
+
+    public static void main(String[] args) {
+      System.out.println( "<!doctype html>\n" );
+      System.out.println ("<!doctype html>\n" );
+      System.out.println( "<html lang='en'>\n" );
+
+      System.out.println( "<head>\n" );
+      System.out.println( "<meta charset='utf-8'>\n" );
+      System.out.println( "<title>Hello World</title>\n" );
+      System.out.println( "</head>\n\n" );
+
+      System.out.println( "<body>\n" );
+      System.out.println( "<h1>Hello World !</h1>\n" );
+      System.out.println( "</body>\n\n" );
+
+      System.out.println( "</html>" );
+    }
+  }
