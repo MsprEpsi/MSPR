3e89fee7bd310548054241a5787277aa7fa4a8af
Mathieu Papazian <mathieu.papazian46@gmail.com>
2022-01-05 17:43:25 +0100

    read and write staff

--- index.java
+++ index.java
@@ -4,48 +4,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
 
 public class index {
     public static void main(String[] args) throws Exception {
-    IndexJava();
-        lecteur();
+        ReadStaff();
     }
-    public static void IndexJava() throws IOException{
-     try (
-        PrintWriter writer = new PrintWriter("index.html","UTF-8")) {
-            writer.print("<!doctype html>\r\n"
-            + "<html lang='en'>\r\n"
-            + " <head>\r\n"
-            + "   <meta charset=\"utf-8\">\r\n"
-            + "   <title>GO Securi</title>\r\n"
-            + " </head>\r\n"
-            + " <body>\r\n"
-            + "   <center>\r\n"
-            + "   <img src=\"C:/Users/Avensys/Documents/perso/MSPR/MSPR/src/logo.png\" alt=\"image\" height=\"100\" />\r\n"
-            + "   </center>\r\n"
-            + "   <u>La liste des Acteurs :</u>\r\n"
-            + " </body>\r\n"
-            + "</html>\r\n"
-            );
-        }
-        BufferedReader br = new BufferedReader(new FileReader("texte.txt"));
+
+    public static void ReadStaff() throws IOException{
+        List<String> listAgent = new ArrayList<String>();
+        URL url = new URL("https://raw.githubusercontent.com/MsprEpsi/MSPR-Client/main/staff.txt");
+        BufferedReader br = new BufferedReader(
+        new InputStreamReader(url.openStream()));
         try{
             String i;
             while ((i = br.readLine()) != null)
-                System.out.println(i);
+                listAgent.add(i);
             br.close();
       }finally{
         br.close();
       }
+      String agents = "";
+      for(int i = 0; i < listAgent.size(); i++){
+            agents = listAgent.get(i)+"\r";
+      
+        try (
+            PrintWriter writer = new PrintWriter(listAgent.get(i)+".html","UTF-8")) {
+                writer.print("<!doctype html>\r\n"
+                + "<html lang='en'>\r\n"
+                + " <head>\r\n"
+                + "   <meta charset=\"utf-8\">\r\n"
+                + "   <title>GO Securi</title>\r\n"
+                + " </head>\r\n"
+                + " <body>\r\n"
+                + "   <center>\r\n"
+                + "   <img src=\"C:/Users/Avensys/Documents/perso/MSPR/MSPR/src/logo.png\" alt=\"image\" height=\"100\" />\r\n"
+                + "   </center>\r\n"
+                + agents
+                + " </body>\r\n"
+                + "</html>\r\n"
+                );
+         }
+        }
+        
       }
-      public static void lecteur() throws Exception {
-        URL url = new URL("https://raw.githubusercontent.com/MsprEpsi/MSPR-Client/main/cbertier.txt");
-        BufferedReader read = new BufferedReader(
-        new InputStreamReader(url.openStream()));
-        String i;
-        while ((i = read.readLine()) != null)
-            System.out.println(i);
-        read.close();
-    }
 
   }
