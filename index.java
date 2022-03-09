8ea8b3646ca155c18e28423dac0e36d8885c614d
Mathieu Papazian <mathieu.papazian46@gmail.com>
2022-03-09 16:53:59 +0100

    add test

--- index.java
+++ index.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+    import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 public class index {
     public static void main(String[] args) throws Exception {
         ReadStaff();
-        // test comment for jenkins job
     }
 
     public static void ReadStaff() throws IOException{
@@ -31,33 +30,37 @@ public class index {
             agentsInfo = "";
             agents = listAgent.get(i)+"\r";
             URL urlAgent = new URL("https://raw.githubusercontent.com/MsprEpsi/MSPR-Client/main/"+listAgent.get(i)+".txt");
-            BufferedReader brAgent = new BufferedReader(
-            new InputStreamReader(urlAgent.openStream()));
-            try{
-                String j;
-                while ((j = brAgent.readLine()) != null)
-                    agentsInfo+=j+"</br>";
-            }finally{
-                brAgent.close();
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            int responseCode = huc.getResponseCode();
+            if(Assert.assertEquals(HttpURLConnection.HTTP_OK, responseCode)){
+                BufferedReader brAgent = new BufferedReader(
+                new InputStreamReader(urlAgent.openStream()));
+                try{
+                    String j;
+                    while ((j = brAgent.readLine()) != null)
+                        agentsInfo+=j+"</br>";
+                }finally{
+                    brAgent.close();
+                }
+                try (
+                    PrintWriter writer = new PrintWriter(listAgent.get(i)+".html","UTF-8")) {
+                        writer.print("<!doctype html>\r\n"
+                        + "<html lang='en'>\r\n"
+                        + " <head>\r\n"
+                        + "   <meta charset=\"utf-8\">\r\n"
+                        + "   <title>GO Securi</title>\r\n"
+                        + " </head>\r\n"
+                        + " <body>\r\n"
+                        + "   <center>\r\n"
+                        + "   <img src=\"https://raw.githubusercontent.com/MsprEpsi/MSPR/main/src/logo.png\" alt=\"image\" height=\"100\" />\r\n"
+                        + "   </center>\r\n"
+                        + agents
+                        + agentsInfo
+                        + " </body>\r\n"
+                        + "</html>\r\n"
+                        );
+                }
             }
-        try (
-            PrintWriter writer = new PrintWriter(listAgent.get(i)+".html","UTF-8")) {
-                writer.print("<!doctype html>\r\n"
-                + "<html lang='en'>\r\n"
-                + " <head>\r\n"
-                + "   <meta charset=\"utf-8\">\r\n"
-                + "   <title>GO Securi</title>\r\n"
-                + " </head>\r\n"
-                + " <body>\r\n"
-                + "   <center>\r\n"
-                + "   <img src=\"https://raw.githubusercontent.com/MsprEpsi/MSPR/main/src/logo.png\" alt=\"image\" height=\"100\" />\r\n"
-                + "   </center>\r\n"
-                + agents
-                + agentsInfo
-                + " </body>\r\n"
-                + "</html>\r\n"
-                );
-         }
         }
         
       }
